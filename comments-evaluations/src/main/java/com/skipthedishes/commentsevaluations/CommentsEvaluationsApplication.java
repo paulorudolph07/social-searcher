df00002aef85ee4210154a36511e8ce300ce4c07
rudolph <paulorudolph07@gmail.com>
2018-06-24 15:37:09 -0300

    initial commit

---
+++ comments-evaluations/src/main/java/com/skipthedishes/commentsevaluations/CommentsEvaluationsApplication.java
@@ -0,0 +1,12 @@
+package com.skipthedishes.commentsevaluations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsEvaluationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsEvaluationsApplication.class, args);
+	}
+}
